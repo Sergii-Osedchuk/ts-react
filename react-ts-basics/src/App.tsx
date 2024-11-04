282de5cdf2b2c0cd419eb156d293199c08797afb
Sergio <norgestream@gmail.com>
2024-11-04 17:37:00 +0200

    add ability to add new goal

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -13,7 +13,14 @@ export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
   const handleAddGoal = () => {
-
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React",
+        description: "Learn it roundabout"
+      };
+      return [...prevGoals, newGoal];
+    })
   }
 
   return (
@@ -21,10 +28,12 @@ export default function App() {
       <Header image={{ src: goalsImg, alt: 'A list of goals' }} >
         <h1>Your Course Goals</h1>
       </Header>
-      <button onClick = {handleAddGoal}>Add goal</button>
-      <CourseGoal title="Learn React + TS">
-        Learn and practise it every day.
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add goal</button>
+      {goals.map(goal => <li key={goal.id}>
+        <CourseGoal title={goal.title}>
+          {goal.description}
+        </CourseGoal>
+      </li>)}
     </main>
   )
 }
